06dc78e86c47c8fc809b0043a3a99c408be5afe8
StevenMena <stevenmena29@gmail.com>
2019-01-24 11:59:02 -0600

    Using Repositories with CrudRepository

---
+++ src/main/java/com/stevomena/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.stevomena.spring5webapp.repositories;
+
+import com.stevomena.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author,Long> {
+}
---
+++ src/main/java/com/stevomena/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package com.stevomena.spring5webapp.repositories;
+
+import com.stevomena.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book,Long> {
+}
