b3cc7f0ff52304b3ecdf1355eaefafefe2bc6462
StevenMena <stevenmena29@gmail.com>
2019-02-04 10:35:45 -0600

    Firt Implementation Bootstrap with AplicationListener

--- src/main/java/com/stevomena/spring5webapp/model/Book.java
+++ src/main/java/com/stevomena/spring5webapp/model/Book.java
@@ -15,7 +15,8 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+    @OneToOne
+    private Publisher publisher;
     @ManyToMany
     //We put jointable annotation and specify the table that this relationship and the column's name, with it gets a one table in
     // author_book and singular names
@@ -25,13 +26,13 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -62,11 +63,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
